c077f22114f49db86019cba230113006abd3c6de
Andrey Gubkin <andreygubkin@mail.ru>
2025-01-09 03:55:57 +0500

    заполнение карты Драйва

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,108 +16,163 @@ fun main(args : Array<String>) {
     val nbAdditionalElevators = input.nextInt() // number of additional elevators that you can build
     val nbElevators = input.nextInt() // number of elevators
 
-    val elevators: List<HashSet<Int>> = List(exitFloor + 1) {
-        hashSetOf()
+    /**
+     * Количество этажей, с которыми имеет смысл работать.
+     * Более верхние этажи можно отбросить, так как с них невозможно добраться до выхода
+     * и клонов на эти этажи пускать не будем
+     */
+    val workFloorsCount = exitFloor + 1
+
+    class Elevators {
+
+        private val elevators: List<HashSet<Int>> = List(workFloorsCount) {
+            hashSetOf()
+        }
+
+        fun registerElevator(
+            floor: Int,
+            position: Int,
+        ) {
+            if (floor <= exitFloor) {
+                elevators[floor] += position
+            }
+        }
+
+        fun isElevator(
+            floor: Int,
+            position: Int,
+        ): Boolean {
+            return position in elevators[floor]
+        }
     }
+
+    val elevators = Elevators()
 
     for (i in 0 until nbElevators) {
         val elevatorFloor = input.nextInt() // floor on which this elevator is found
         val elevatorPos = input.nextInt() // position of the elevator on its floor
 
-        if (elevatorFloor <= exitFloor) {
-            elevators[elevatorFloor] += elevatorPos
-        }
-    }
-
-    var mapIsBuilt = false
-
-    fun ensureDriveMap() {
-        if (mapIsBuilt) {
-            return
-        }
-
-        fun buildEmptyCasesForFloor(): List<MutableList<Case>> {
-            return List(size = width) {
-                mutableListOf()
-            }
-        }
-
-        fun buildExitFloorCases(): List<List<Case>> {
-            val cases = buildEmptyCasesForFloor()
-            var position = exitPos
-
-            fun addCases(vararg newCases: Case) {
-                cases[position] += newCases
-            }
-
-            addCases(
-                Case(direction = Direction.LEFT, distance = 0, elevatorsReserved = 0, clonesReserved = 0),
-                Case(direction = Direction.RIGHT, distance = 0, elevatorsReserved = 0, clonesReserved = 0),
+        elevators
+            .registerElevator(
+                floor = elevatorFloor,
+                position = elevatorPos,
             )
+    }
 
-            position = exitPos - 1
-            while (position >= 0 && position !in elevators[exitFloor]) {
-                // так как этаж с выходом, то не нужен запас лифтов
-                addCases(
-                    // если клон бежит влево, а выход справа, то помимо расстояния до выхода
-                    // надо ещё и развернуться - заблокировав одного клона из резерва
-                    Case(
-                        direction = Direction.LEFT,
-                        distance = exitPos - position + 1,
-                        elevatorsReserved = 0,
-                        clonesReserved = 1,
-                    ),
-                    // если клон бежит вправо и выход справа, то нужно только
-                    // пробежать расстояние до выхода без расходования клонов
-                    Case(
-                        direction = Direction.RIGHT,
-                        distance = exitPos - position,
-                        elevatorsReserved = 0,
-                        clonesReserved = 0,
-                    ),
-                )
-                position--
-            }
-
-            position = exitPos + 1
-            while (position < width && position !in elevators[exitFloor]) {
-                // так как этаж с выходом, то не нужен запас лифтов
-                addCases(
-                    // если клон бежит вправо, а выход слева, то помимо расстояния до выхода
-                    // надо ещё и развернуться - заблокировав одного клона из резерва
-                    Case(
-                        direction = Direction.RIGHT,
-                        distance = position - exitPos + 1,
-                        elevatorsReserved = 0,
-                        clonesReserved = 1,
-                    ),
-                    // если клон бежит влево и выход влево, то нужно только
-                    // пробежать расстояние до выхода без расходования клонов
-                    Case(
-                        direction = Direction.LEFT,
-                        distance = position - exitPos,
-                        elevatorsReserved = 0,
-                        clonesReserved = 0,
-                    ),
-                )
-                position++
-            }
-
-            return cases
+    class Floor(
+        val floorIndex: Int,
+    ) {
+        private val floorCases: List<MutableList<Case>> = List(size = width) {
+            mutableListOf()
         }
 
-        val exitFloorCases = buildExitFloorCases()
+        val cases: List<List<Case>> get() = floorCases
 
-
-
-        mapIsBuilt = true
+        fun addCases(
+            position: Int,
+            vararg newCases: Case,
+        ) {
+            floorCases[position] += newCases
+        }
     }
+
+    class Drive(
+        val floors: List<Floor>,
+    ) {
+
+    }
+
+    /**
+     * Варианты движения для каждого этажа и каждой ячейки на этаже
+     */
+    fun buildDrive(): Drive {
+        fun buildExitFloor(): Floor {
+            return Floor(floorIndex = exitFloor)
+                .apply {
+                    addCases(
+                        position = exitPos,
+                        Case(direction = Direction.LEFT, distance = 0, elevatorsReserved = 0, clonesReserved = 0),
+                        Case(direction = Direction.RIGHT, distance = 0, elevatorsReserved = 0, clonesReserved = 0),
+                    )
+
+                    (exitPos - 1 downTo 0)
+                        .takeWhile { position ->
+                            !elevators.isElevator(floor = floorIndex, position = position)
+                        }
+                        .forEach { position ->
+                            // так как этаж с выходом, то не нужен запас лифтов
+                            addCases(
+                                position = position,
+                                // если клон бежит влево, а выход справа, то помимо расстояния до выхода
+                                // надо ещё и развернуться - заблокировав одного клона из резерва
+                                Case(
+                                    direction = Direction.LEFT,
+                                    distance = exitPos - position + 1,
+                                    elevatorsReserved = 0,
+                                    clonesReserved = 1,
+                                ),
+                                // если клон бежит вправо и выход справа, то нужно только
+                                // пробежать расстояние до выхода без расходования клонов
+                                Case(
+                                    direction = Direction.RIGHT,
+                                    distance = exitPos - position,
+                                    elevatorsReserved = 0,
+                                    clonesReserved = 0,
+                                ),
+                            )
+                        }
+
+                    (exitPos + 1 until width)
+                        .takeWhile { position ->
+                            !elevators.isElevator(floor = floorIndex, position = position)
+                        }
+                        .forEach { position ->
+                            // так как этаж с выходом, то не нужен запас лифтов
+                            addCases(
+                                position = position,
+                                // если клон бежит вправо, а выход слева, то помимо расстояния до выхода
+                                // надо ещё и развернуться - заблокировав одного клона из резерва
+                                Case(
+                                    direction = Direction.RIGHT,
+                                    distance = position - exitPos + 1,
+                                    elevatorsReserved = 0,
+                                    clonesReserved = 1,
+                                ),
+                                // если клон бежит влево и выход слева, то нужно только
+                                // пробежать расстояние до выхода без расходования клонов
+                                Case(
+                                    direction = Direction.LEFT,
+                                    distance = position - exitPos,
+                                    elevatorsReserved = 0,
+                                    clonesReserved = 0,
+                                ),
+                            )
+                        }
+                }
+        }
+
+        val exitFloorCases = buildExitFloor()
+
+        return Drive(
+           floors = generateSequence(
+               seed = exitFloorCases,
+           ) { previousFloor ->
+               Floor(
+                   floorIndex = previousFloor.floorIndex - 1,
+               ).apply {
+                   TODO()
+               }
+           }
+               .take(workFloorsCount)
+               .toList()
+               .reversed()
+        )
+    }
+
+    val driveMap = buildDrive()
 
     // game loop
     while (true) {
-
-        ensureDriveMap()
-
         val cloneFloor = input.nextInt() // floor of the leading clone
         val clonePos = input.nextInt() // position of the leading clone on its floor
         val direction = input.next() // direction of the leading clone: LEFT or RIGHT
